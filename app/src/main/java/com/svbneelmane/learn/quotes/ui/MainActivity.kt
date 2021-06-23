ea9d903fcf5263b020242a31ca08bc570ab96380
Shivaprasad Bhat <svbhat.neelmane@gmail.com>
2021-06-23 22:40:13 +0530

    Adding Timber implementations and Comments in MainActivity.kt

--- app/src/main/java/com/svbneelmane/learn/quotes/ui/MainActivity.kt
+++ app/src/main/java/com/svbneelmane/learn/quotes/ui/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.svbneelmane.learn.quotes.ui
 
+/**
+ * Entry Point to the Application
+ * @author Shivaprasad Bhat
+ * @date 23-Jun-2021
+ */
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +13,7 @@ import com.svbneelmane.learn.quotes.data.Quotes
 import com.svbneelmane.learn.quotes.databinding.ActivityMainBinding
 import com.svbneelmane.learn.quotes.viewmodels.MainViewModel
 import com.svbneelmane.learn.quotes.viewmodels.MainViewModelFactory
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -15,9 +21,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Timber.plant(Timber.DebugTree())
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Create ViewModel using the factory class
         viewModel = ViewModelProvider(
             this,
             MainViewModelFactory(application)
@@ -25,6 +33,9 @@ class MainActivity : AppCompatActivity() {
 
         setQuote(viewModel.getQuote())
 
+        /**
+         * Set On Click Listeners to Prev, Next and Share buttons
+         */
         with(binding) {
             tvPrevious.setOnClickListener {
                 onPrevious()
@@ -39,6 +50,10 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    /**
+     * Function that sets the quotes on the UI
+     * Uses ViewBinding
+     */
     private fun setQuote(quotes: Quotes) {
         with(binding) {
             quoteText.text = quotes.text
@@ -46,14 +61,24 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Function that acts on click of Previous button
+     */
     private fun onPrevious() {
         setQuote(viewModel.prevQuote())
     }
 
+    /**
+     * Function that acts on click of Next button
+     */
     private fun onNext() {
         setQuote(viewModel.nextQuote())
     }
 
+
+    /**
+     * Function that acts on click of Share floating button
+     */
     private fun onShare() {
         val intent = Intent(Intent.ACTION_SEND)
         intent.type = "text/plain"
